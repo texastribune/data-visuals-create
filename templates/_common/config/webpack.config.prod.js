307a615a8cef5030d3e43e50c4ac190d393b6904
Ryan Murphy <rdmurphy@users.noreply.github.com>
2018-02-15 14:32:59 -0600

    Add HashedModuleIdsPlugin plugin to prod

--- templates/_common/config/webpack.config.prod.js
+++ templates/_common/config/webpack.config.prod.js
@@ -24,6 +24,7 @@ productionConfig.output = {
 
 productionConfig.plugins.push(
   new webpack.optimize.ModuleConcatenationPlugin(),
+  new webpack.HashedModuleIdsPlugin(),
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify('production'),
