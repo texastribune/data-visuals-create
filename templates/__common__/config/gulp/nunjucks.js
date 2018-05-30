c3b3f44d1f4f73519c08771320a980dec16c6556
Ryan Murphy <rdmurphy@users.noreply.github.com>
2018-05-30 13:54:07 -0500

    Add jsonScript to nunjucks

--- templates/__common__/config/gulp/nunjucks.js
+++ templates/__common__/config/gulp/nunjucks.js
@@ -109,6 +109,30 @@ env.addGlobal('CURRENT_YEAR', new Date().getFullYear());
 env.addGlobal('parseData', parseData);
 
 /*
+Add `json_script` filter.
+ */
+const _jsonScriptMap = {
+  '>': '\\u003E',
+  '<': '\\u003C',
+  '&': '\\u0026',
+};
+
+const escapeHtmlRegex = new RegExp(
+  `[${Object.keys(_jsonScriptMap).join('')}]`,
+  'g'
+);
+
+function escapeHtml(text) {
+  return text.replace(escapeHtmlRegex, m => _jsonScriptMap[m]);
+}
+
+env.addFilter('jsonScript', (value, elementId) => {
+  return `<script id="${elementId}" type="application/json">${escapeHtml(
+    JSON.stringify(value)
+  )}</script>`;
+});
+
+/*
 Set up `journalize`.
  */
 for (let key in journalize) {
