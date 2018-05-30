7460031e2fab25567ae62cbf0629f04d7e339f79
Ryan Murphy <rdmurphy@users.noreply.github.com>
2018-05-30 14:01:43 -0500

    Add helper for loading jsonScript inlines

---
+++ templates/__common__/app/scripts/utils/load-json-script.js
@@ -0,0 +1,17 @@
+/**
+ * Grabs the contents of a `jsonScript` created JSON inline string and returns
+ * it as a JSON object. If the element does not exist, or if the script
+ * element does not have the JSON MIME type, null is returned instead.
+ *
+ * @param  {String} elementId the ID of the JSON script element
+ * @return {Object}
+ */
+export default function loadJsonScript(elementId) {
+  const element = document.getElementById(elementId);
+
+  if (element && element.getAttribute('type') === 'application/json') {
+    return JSON.parse(element.textContent);
+  } else {
+    return null;
+  }
+}
