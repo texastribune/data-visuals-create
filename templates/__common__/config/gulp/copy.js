87c6031b315cb561b8cf39b3b4c0fe66f19d3dcb
Ryan Murphy <rdmurphy@users.noreply.github.com>
2018-08-27 19:11:28 -0500

    BUGFIX: copy task should not crash if assets dir does not exist

--- templates/__common__/config/gulp/copy.js
+++ templates/__common__/config/gulp/copy.js
@@ -1,4 +1,5 @@
 // packages
+const chalk = require('chalk');
 const fs = require('fs-extra');
 
 // internal
@@ -6,8 +7,12 @@ const { isImagePath } = require('./utils');
 const paths = require('../paths');
 
 module.exports = async () => {
-  await fs.copy(paths.appAssets, paths.appDistAssets, {
-    dereference: true,
-    filter: src => !isImagePath(src),
-  });
+  if (await fs.pathExists(paths.appAssets)) {
+    await fs.copy(paths.appAssets, paths.appDistAssets, {
+      dereference: true,
+      filter: src => !isImagePath(src),
+    });
+  } else {
+    console.warn(chalk.grey`No "assets" directory found, skipping copy task.`);
+  }
 };
