1d11832d515613a81280b14717e2fbade0bcb9aa
Ryan Murphy <rdmurphy@users.noreply.github.com>
2019-02-28 11:12:05 -0600

    Better starter default for graphics

--- templates/graphic/app/scripts/graphic.js
+++ templates/graphic/app/scripts/graphic.js
@@ -1,13 +1,28 @@
+import * as d3 from 'd3';
+
+// a reference to the default graphic container, change if needed
+const container = d3.select('#graphic');
+
+// a helper function to clear the container of its contents
+const clearContainer = () => container.html('');
+
+// a helper function to grab the container's width
+const getFrameWidth = () => container.node().offsetWidth;
+
 /**
  * This function is called to render a graphic by Pym.js. The frame's width and
  * a reference to the Pym instance is provided.
  *
- * @param  {Number} [frameWidth=600] The current width of the iframe, defaults
- *                                   to 600px to help with development
- * @param  {Object} pymChild         A reference to the IFrame's Pym instance
+ * @param  {Object} pymChild         A reference to the iframe's Pym instance
  * @return {void}
  */
-export default function renderGraphic(frameWidth, pymChild) {
+export default function renderGraphic(pymChild) {
+  // immediately clear the container on each render
+  clearContainer();
+
+  // get the width of the container
+  const frameWidth = getFrameWidth();
+
   // Any code neccessary to render your graphic should be placed here - this
   // function is what is called in main.js by Pym
 }
