65d4cf04967e221d3aa919f65699ceb92a7a82e8
Mandi Cai <mcai@texastribune.org>
2019-09-18 12:13:13 -0500

    Remove pym and import frameLoader() to activate the responsive iframe #17

--- templates/graphic/app/scripts/packs/main.js
+++ templates/graphic/app/scripts/packs/main.js
@@ -1,26 +1,6 @@
-import * as d3 from 'd3';
-import { pymLoader } from '../embeds/pym';
+import { frameLoader } from '../embeds/frames';
+import renderGraphic from './graphic';
 
-// a reference to the default graphic container, change if needed
-const container = d3.select('#graphic');
-
-// a helper function to clear the container of its contents
-const clearContainer = () => container.html('');
-
-// a helper function to grab the container's width
-const getFrameWidth = () => container.node().offsetWidth;
-
-/**
- * This function is called to render a graphic by Pym.js. The frame's width and
- * a reference to the Pym instance is provided.
- *
- * @param  {Object} pymChild         A reference to the iframe's Pym instance
- * @return {void}
- */
-export default function renderGraphic(pymChild) {
-  // uncomment these two lines if you're creating a coded graphic
-  // clearContainer(); // clears the container
-  //const frameWidth = getFrameWidth(); // calcuates the width on each render
-}
-
-pymLoader(renderGraphic).catch(console.error);
+// This JS pack is included by default, so our graphics can be wrapped in AMP-compatible iframes
+// renderGraphic() renders a coded graphic on resize
+frameLoader(renderGraphic);
