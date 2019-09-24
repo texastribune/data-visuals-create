edc94e7e917ee044d8fce22816eb569e9d952775
Mandi Cai <mcai@texastribune.org>
2019-09-23 19:17:19 -0500

    Edit README, rename template and script files for non-scripted graphics #17

--- templates/graphic/app/scripts/packs/main.js
+++ templates/graphic/app/scripts/packs/main.js
@@ -1,7 +1,7 @@
 import { frameLoader } from '../embeds/frames';
 import renderGraphic from './graphic';
 
-// index.html includes this pack by default
+// included in index.html by default
 // initiates frame so your graphic is wrapped in an AMP-compatible iframe
 // renderGraphic() renders a coded graphic on load and on resize
 frameLoader(renderGraphic);
--- templates/graphic/app/scripts/packs/static.js
+++ templates/graphic/app/scripts/packs/static.js
@@ -1,5 +1,5 @@
 import { frameLoader } from '../embeds/frames';
 
-// ai.html includes this pack by default
+// included in static.html by default
 // initiates frame so your graphic is wrapped in an AMP-compatible iframe
 frameLoader();
