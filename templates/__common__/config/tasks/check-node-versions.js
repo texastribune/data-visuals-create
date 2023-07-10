c59f81e95eb0d6a03a89b1d3a294a5967ad48331
yurikoschumacher <yuriko.schumacher@gmail.com>
2023-07-10 17:53:50 -0500

    add a script to check node versions and update package.json scripts

---
+++ templates/__common__/config/tasks/check-node-versions.js
@@ -0,0 +1,11 @@
+// packages
+const colors = require('ansi-colors');
+
+// get node versions
+const version = process.version;
+const versionNumeric = Number(process.versions.node.split('.')[0]);
+
+// show this error message if the node version is 16 or older
+if (versionNumeric <= 16) {
+  console.log(`\n\n${colors.bgMagenta("ERROR!")} You are using node version ${colors.bold(colors.red(version))}.\ndata-visuals-create only supports node versions 18 or later.\nUpdate your node version by running ${colors.bold(colors.yellow("nvm install 18"))} or ${colors.bold(colors.yellow("nvm install node"))} in your command line.\n\n`)
+}
