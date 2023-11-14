8731fb6c154d164d75d13e24663d7981290a10f8
yurikoschumacher <yuriko.schumacher@gmail.com>
2023-11-14 10:51:20 -0600

    cleaned formatting

--- templates/__common__/config/tasks/parse-figma2html.js
+++ templates/__common__/config/tasks/parse-figma2html.js
@@ -12,25 +12,25 @@ const moveHtml = filename => {
   fs.renameSync(
     'workspace/' + filename + '.html',
     'app/templates/figma2html-output/' + filename + '.html'
-  )
-}
+  );
+};
 
 const moveImages = filename => {
   // move the image files in the app/assets/figma2html/FILENAME directory
   const imagesPath = 'workspace/assets/figma2html/' + filename;
   fs.readdir(imagesPath, (err, images) => {
     images.forEach(image => {
-      console.log('moving ' + image + '...')
+      console.log('moving ' + image + '...');
       fs.renameSync(
         imagesPath + '/' + image,
-        'app/assets/figma2html/' + filename + '/' + image,
-      )
-    })
-  })
-}
+        'app/assets/figma2html/' + filename + '/' + image
+      );
+    });
+  });
+};
 
 const moveFiles = filename => {
-  console.log('Now moving the figma2html files...')
+  console.log('Now moving the figma2html files...');
 
   // check if there's app/templates/figma2html-output directory
   const templatesPath = 'app/templates/figma2html-output';
@@ -38,12 +38,12 @@ const moveFiles = filename => {
     fs.accessSync(templatesPath);
   } catch (err) {
     fs.mkdirSync(templatesPath, err => {
-      if(err) {
-        console.log(err)
+      if (err) {
+        console.log(err);
       } else {
-        console.log("created figma2html-output directory!")
+        console.log('created figma2html-output directory!');
       }
-    })
+    });
   } finally {
     moveHtml(filename);
   }
@@ -53,64 +53,64 @@ const moveFiles = filename => {
 
   try {
     fs.accessSync(assetsPath);
-    console.log("figma2html directory already exists!")
+    console.log('figma2html directory already exists!');
   } catch (err) {
-    fs.mkdirSync(assetsPath, (err) => {
-      if(err) {
-        console.log(err)
+    fs.mkdirSync(assetsPath, err => {
+      if (err) {
+        console.log(err);
       } else {
-        console.log("created new figma2html directory!")
+        console.log('created new figma2html directory!');
       }
-    })
+    });
   } finally {
     // create app/assets/figma2html/FILENAME directory
     try {
       fs.accessSync(assetsPath + filename);
-      console.log("app/assets/figma2html/" + filename + " already exists!")
-    } catch(err) {
-      fs.mkdirSync(assetsPath + filename, (err) => {
-        if(err) {
-          console.log(err)
+      console.log('app/assets/figma2html/' + filename + ' already exists!');
+    } catch (err) {
+      fs.mkdirSync(assetsPath + filename, err => {
+        if (err) {
+          console.log(err);
         } else {
-          console.log("created figma2html/FILENAME directory!")
+          console.log('created figma2html/FILENAME directory!');
         }
-      })
+      });
     }
     moveImages(filename);
   }
-}
+};
 
-const extractZip = async (file) => {
-    // extract the zip file
-    try {
-      console.log('Unzipping ' + colors.magentaBright(file) + '...')
-      await extract(
-        resolve('workspace/' + file),
-        // save the extracted zip file in the workspace directory
-        { dir: resolve('workspace') }
-      )
-      console.log(colors.magentaBright(file) + ' Extraction completed');
-    } catch(err) {
-      // handle any errors
-    } finally {
-      // get the file name
-      const filename = file.replace('.zip', '');
-      // then move files
-      moveFiles(filename);
-    }
-}
+const extractZip = async file => {
+  // extract the zip file
+  try {
+    console.log('Unzipping ' + colors.magentaBright(file) + '...');
+    await extract(
+      resolve('workspace/' + file),
+      // save the extracted zip file in the workspace directory
+      { dir: resolve('workspace') }
+    );
+    console.log(colors.magentaBright(file) + ' Extraction completed');
+  } catch (err) {
+    // handle any errors
+  } finally {
+    // get the file name
+    const filename = file.replace('.zip', '');
+    // then move files
+    moveFiles(filename);
+  }
+};
 
 module.exports = () => {
-  // check if a zip file exists in the workspace directory 
+  // check if a zip file exists in the workspace directory
   fs.readdir('workspace', (err, files) => {
     files.forEach(file => {
-      if(file.includes('assets')) {
+      if (file.includes('assets')) {
         // remove old assets files
         fs.rmSync(file, { recursive: true, force: true });
       } else if (file.includes('.zip')) {
         // extract the zip file
-        extractZip(file)
-      } 
-    })
-  })
-}
+        extractZip(file);
+      }
+    });
+  });
+};
